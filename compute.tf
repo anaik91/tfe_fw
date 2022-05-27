021449b8974a277b5cb9b2dc8677cc2f35361481
Ashwin Naik <ashwinknaik@google.com>
2022-05-27 10:52:29 +0530

    success push

--- compute.tf
+++ compute.tf
@@ -26,31 +26,3 @@ resource "google_compute_instance" "instance1" {
   }
 
 }
-
-resource "google_compute_instance" "instance2" {
-  name         = "test2"
-  machine_type = "e2-medium"
-  zone         = "us-central1-a"
-
-  tags = ["foo", "bar"]
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
-    }
-  }
-
-  // Local SSD disk
-  scratch_disk {
-    interface = "SCSI"
-  }
-
-  network_interface {
-    network = google_compute_network.default.name
-  }
-
-  metadata = {
-    block-project-ssh-keys = "false"
-  }
-
-}
--- subnet.tf
+++ subnet.tf
@@ -7,33 +7,6 @@ resource "google_compute_subnetwork" "subnet1" {
     range_name    = "tf-test-secondary-range-update1"
     ip_cidr_range = "192.168.10.0/24"
   }
-}
-
-resource "google_compute_subnetwork" "subnet2" {
-  name          = "test-subnetwork"
-  ip_cidr_range = "10.2.0.0/16"
-  region        = "us-central1"
-  network       = google_compute_network.default.id
-  secondary_ip_range {
-    range_name    = "tf-test-secondary-range-update1"
-    ip_cidr_range = "192.168.10.0/24"
-  }
-
-  log_config {
-    aggregation_interval = "INTERVAL_5_SEC"
-    metadata             = "EXCLUDE_ALL_METADATA"
-  }
-}
-
-resource "google_compute_subnetwork" "subnet3" {
-  name          = "test-subnetwork"
-  ip_cidr_range = "10.2.0.0/16"
-  region        = "us-central1"
-  network       = google_compute_network.default.id
-  secondary_ip_range {
-    range_name    = "tf-test-secondary-range-update1"
-    ip_cidr_range = "192.168.10.0/24"
-  }
   private_ip_google_access = true
   log_config {
     aggregation_interval = "INTERVAL_5_SEC"
